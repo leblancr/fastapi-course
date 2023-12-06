eafaa46579987fd70f0be39ebec0f4f7be86792d
rich leblanc <rich.leblanc1@proton.me>
2023-12-06 10:40:14 -0500

    11 - added pydantic

---
+++ README.md
@@ -0,0 +1,2 @@
+Python API Course
+https://www.youtube.com/watch?v=PuQzQNJZ4c8&list=PL8VzFQ8k4U1L5QpSapVEzoSfob-4CR8zM&index=10
---
+++ main.py
@@ -0,0 +1,40 @@
+import logging
+from typing import Optional
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(levelname)s - %(message)s'
+                    )
+
+logging.debug('Debug')
+logging.info('info')
+logging.warning('warning')
+logging.error('error')
+logging.critical('critical')
+
+app = FastAPI()
+
+
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True  # optional
+    rating: Optional[int] = None
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.post("/createposts")
+async def create_posts(post: Post):
+    logging.info(f"post {post}")
+    logging.info(f"post.title: {post.title}")
+    logging.info(f"post.content: {post.content}")
+    logging.info(f"post.published: {post.published}")
+    logging.info(f"post.rating: {post.rating}")
+    logging.info(f"post.dict: {post.dict()}")
+    
+    return {"data": post}
