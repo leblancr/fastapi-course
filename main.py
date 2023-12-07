df787cfcb57a37022bd1622f8575e2794d4ab498
rich leblanc <rich.leblanc1@proton.me>
2023-12-07 17:20:54 -0500

    17 - added response status codes

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import logging
 from typing import Optional
-from fastapi import FastAPI
+from fastapi import FastAPI, Response, status, HTTPException
 from pydantic import BaseModel
+from random import randrange
 
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s - %(levelname)s - %(message)s'
@@ -23,7 +24,14 @@ class Post(BaseModel):
     rating: Optional[int] = None
 
 
-my_posts = {}
+my_posts = [{'title': 'title of post 1', 'content': 'content of post 1', 'id': 1},
+            {'title': 'fovorite foods', 'content': 'pizza', 'id': 2}]
+
+
+def find_post(id):
+    for p in my_posts:
+        if p['id'] == id:
+            return p
 
 
 @app.get("/")
@@ -31,13 +39,35 @@ async def root():
     return {"message": "Hello World"}
 
 
-@app.post("/createposts")
+# Get one post, pydantic type check converts string id to int.
+@app.get("/posts/{id}")  # string id
+async def get_post(id: int):  # string gets converted to int
+    logging.info(f"Getting post id {id}")
+    post = find_post(id)  # int required
+
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"post with id: {id} not found")
+    return {"post_detail": post}
+
+
+# Get all posts
+@app.get("/posts")
+async def get_posts():
+    return {"data": my_posts}
+
+
+@app.post("/posts", status_code=status.HTTP_201_CREATED)
 async def create_posts(post: Post):
     logging.info(f"post {post}")
     logging.info(f"post.title: {post.title}")
     logging.info(f"post.content: {post.content}")
     logging.info(f"post.published: {post.published}")
     logging.info(f"post.rating: {post.rating}")
-    logging.info(f"post.dict: {post.dict()}")
-    
-    return {"data": post}
+    logging.info(f"post.dict: {post.model_dump()}")
+
+    post_dict = post.model_dump()
+    post_dict['id'] = randrange(0, 1000000)
+    my_posts.append(post_dict)
+
+    return {"data": post_dict}
